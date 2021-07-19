5169e1dcc396068d5c30dbdf16d10b33139dc403
SushanGhimire <sushangmi55@gmail.com>
2021-07-19 18:20:07 +0545

    delegate.txt is added

--- Program.cs
+++ Program.cs
@@ -2,60 +2,21 @@
 
 namespace testing
 {
-    class Student
+    public delegate void testingDelegate(); // decelaration 
+    class handelingDelegate
     {
-        private int rollno;
-        private double total = 0.0;
-        public int Roll
+        public static void showme()
         {
-            get { return rollno; }
-            set { rollno = value; }
+            System.Console.WriteLine("I am called by delegate");
         }
-        private int[] marks = new int[3]; //array declaration of length 3
-        public int this[int index] // targeting the index of array
-        {
-            get { return marks[index]; }
-            set { marks[index] = value; }
-        }
-        public double Percentage()
-        {
-
-            foreach (int i in marks)
-            {
-                total = total + i; //adding the array numbers
-            }
-            return total / marks.Length; //returning the percentage
-        }
-        public string statusCheck()
-        {
-            // checking  the student  whose % is less than 40 are fail and greater than 40 are pass
-            return total / marks.Length < 40 ? "Fail" : "Pass"; //shortcut using ternary operator
-
-            //or you can also do
-
-            // if (total / marks.Length < 40)
-            //     return "Fail";
-            // else
-            //     return "Pass";
-
-
-        }
-    }
-    class run
-    {
         static void Main(string[] args)
         {
-            Student s1 = new Student();
-            s1[0] = 80;
-            s1[1] = 70;
-            s1[2] = 50;
-            Console.WriteLine("Percentage of Roll no {0} is {1}% and {2}", s1.Roll = 1, s1.Percentage(), s1.statusCheck());
-            Student s2 = new Student();
-            s2[0] = 30;
-            s2[1] = 40;
-            s2[2] = 30;
-            Console.WriteLine("Percentage of Roll no {0} is {1}% and {2}", s2.Roll = 2, s2.Percentage(), s2.statusCheck());
+            testingDelegate t = new testingDelegate(showme);
+            t();
 
+            //delegate is simply like a function pointer. whenever a event is triger delegate tells which methods to call.
+
+            //firstly me made the delegate  and when we made the object of delegate we pass the showme method to it. basically what it does is when program run program will create the new object of delegate and since we have pass pointer to showme() method inside the delegate object it will automatically trigger the showme() method and run the command inside the showme().
         }
     }
 }
