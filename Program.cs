d4d48d140903682d510ec480f31a5d0146f53297
SushanGhimire <sushangmi55@gmail.com>
2021-07-19 12:54:25 +0545

    indexer.txt added

--- Program.cs
+++ Program.cs
@@ -2,35 +2,60 @@
 
 namespace testing
 {
-    class ClassProperty
+    class Student
     {
-        private int num1;
-        private int num2;
+        private int rollno;
+        private double total = 0.0;
+        public int Roll
+        {
+            get { return rollno; }
+            set { rollno = value; }
+        }
+        private int[] marks = new int[3]; //array declaration of length 3
+        public int this[int index] // targeting the index of array
+        {
+            get { return marks[index]; }
+            set { marks[index] = value; }
+        }
+        public double Percentage()
+        {
 
-        public int setNum1
-        {
-            get { return num1; } //gettting variable from above
-            set { num1 = value; } //setting the value to the variable
+            foreach (int i in marks)
+            {
+                total = total + i; //adding the array numbers
+            }
+            return total / marks.Length; //returning the percentage
         }
-        public int setNum2
+        public string statusCheck()
         {
-            get { return num2; }  //gettting variable from above
-            set { num2 = value; } //setting the value to the variable
-        }
-        public void display()
-        {
-            Console.WriteLine("Numbers are {0} and {1} ", num1, num2);
+            // checking  the student  whose % is less than 40 are fail and greater than 40 are pass
+            return total / marks.Length < 40 ? "Fail" : "Pass"; //shortcut using ternary operator
+
+            //or you can also do
+
+            // if (total / marks.Length < 40)
+            //     return "Fail";
+            // else
+            //     return "Pass";
+
+
         }
     }
     class run
     {
         static void Main(string[] args)
         {
-            ClassProperty c = new ClassProperty();
-            c.setNum1 = 10;
-            c.setNum2 = 20;
-            c.display();
+            Student s1 = new Student();
+            s1[0] = 80;
+            s1[1] = 70;
+            s1[2] = 50;
+            Console.WriteLine("Percentage of Roll no {0} is {1}% and {2}", s1.Roll = 1, s1.Percentage(), s1.statusCheck());
+            Student s2 = new Student();
+            s2[0] = 30;
+            s2[1] = 40;
+            s2[2] = 30;
+            Console.WriteLine("Percentage of Roll no {0} is {1}% and {2}", s2.Roll = 2, s2.Percentage(), s2.statusCheck());
+
         }
     }
-
 }
