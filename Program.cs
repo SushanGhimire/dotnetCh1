62ed75c331204332f4b2abff01293d27e1d93037
SushanGhimire <sushangmi55@gmail.com>
2021-07-19 19:09:47 +0545

    2 linq example added

--- Program.cs
+++ Program.cs
@@ -1,41 +1,47 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 namespace testing
 {
     class Program
     {
+        public class Student
+        {
+            public int Id { get; set; }
+            public string name { get; set; }
+            public int age { get; set; }
+        }
         static void Main(string[] args)
         {
-            List<string> names = new List<string>();
-            List<int> numbers = new List<int>();
-
-            names.Add("A");
-            names.Add("B");
-            names.Add("C");
-            names.Add("D");
-            names.Add("E");
-            names.Add("F");
-            names.Add("G");
-            names.Add("H");
-
-            numbers.Add(1);
-            numbers.Add(2);
-            numbers.Add(3);
-            numbers.Add(4);
-            numbers.Add(5);
-            numbers.Add(6);
-            numbers.Add(7);
-            System.Console.WriteLine("..........................");
-            foreach (string n in names)
-                System.Console.WriteLine(n);
-            System.Console.WriteLine("..........................");
-            foreach (int n in numbers)
-                System.Console.WriteLine(n);
-            System.Console.WriteLine("..........................");
-
-
+            //declearing  the array
+            List<Student> sutdentList = new List<Student>()
+            {
+                new Student (){
+                    Id=1,name="A", age=12
+                },
+                 new Student (){
+                    Id=2,name="B", age=20
+                },
+                 new Student (){
+                    Id=3,name="C", age=17
+                },
+                 new Student (){
+                    Id=4,name="D", age=22
+                },
+            };
+            var teenager = from s in sutdentList
+                           where s.age > 12 && s.age < 18
+                           select s;
+            var adult = from s in sutdentList
+                        where s.age > 18
+                        select s;
+            System.Console.WriteLine("Teenagers");
+            foreach (var n in teenager)
+                System.Console.WriteLine("Age of {0} is {1}", n.name, n.age);
+            System.Console.WriteLine("Adults");
+            foreach (var n in adult)
+                System.Console.WriteLine("Age of {0} is {1}", n.name, n.age);
         }
+
     }
 }
-
-// donot forget to import "using System.Collections.Generic" at top; 
